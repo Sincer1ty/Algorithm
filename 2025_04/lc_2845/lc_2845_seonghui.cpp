2ec46d3e2f4adfce7f1ce17890fd17e127a00469
Sincer1ty <woodsy0417@gmail.com>
2025-04-27 21:26:57 +0900

    Update lc_2845_seonghui.cpp

--- 2025_04/lc_2845/lc_2845_seonghui.cpp
+++ 2025_04/lc_2845/lc_2845_seonghui.cpp
@@ -1,50 +1,36 @@
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
-private:
-	int countFromIndex(int start, vector<bool> v, int cnt){
-		int temp = cnt, result = 0;
-
-		for (int i = start; i < v.size(); i++) {
-			if (temp < 0) {
-				break;
-			}
-			if (v[i]) {
-				temp -= 1;
-			}
-			if (temp == 0) result += 1;
-		}
-
-		return result;
-	}
 public:
 	long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
-		int cnt = 0, result = 0;
+		int result = 0;
 		vector<int> validCnts;
-		vector<bool> numsFlag;
-
-		// 1. cnt 범위 찾기
+		vector<vector<int>> matrix(nums.size()+1, vector<int>(nums.size()+1, 0));
+		unordered_map<int, int> dict;
+		
 		if (k == 0) validCnts.push_back(0);
-
-		for (int i = 0; i < nums.size(); i++) {
-			if (nums[i] % modulo == k) {
-				numsFlag.push_back(true);
-				cnt += 1;
-				// 2. 가능성있는 cnt 찾기
-				if (cnt % modulo == k) validCnts.push_back(cnt);
+		
+		for (int i = 1; i <= nums.size(); i++) {
+			for (int j = i; j <= nums.size(); j++) {
+				if (nums[j-1] % modulo == k) {
+					matrix[i][j] = matrix[i][j-1] + 1;
+					if (matrix[i][j] % modulo == k) {
+						validCnts.push_back(matrix[i][j]);
+					}
+				}
+				else matrix[i][j] = matrix[i][j-1];
+				if (dict.find(matrix[i][j]) == dict.end()) {
+					dict[matrix[i][j]] = 1;
+				}
+				else dict[matrix[i][j]] += 1;
 			}
-			else numsFlag.push_back(false);
 		}
 
-		// 3. 해당 cnt에 부합하는 범위 개수 구하기
-		// 인덱스로 참인 것들 기준으로 경우의 수를 찾기
 		for (int i = 0; i < validCnts.size(); i++) {
-			cnt = validCnts[i];
-			for (int j = 0; j < numsFlag.size(); j++) {
-				result += countFromIndex(j, numsFlag, validCnts[i]);
-			}
+			result += dict[validCnts[i]];
 		}
 
 		return result;
