6827707bfba75696efd691ad677ed143a7751c53
Sincer1ty <woodsy0417@gmail.com>
2025-04-28 15:08:17 +0900

    3rd : Update lc_2845_seonghui.cpp

--- 2025_04/lc_2845/lc_2845_seonghui.cpp
+++ 2025_04/lc_2845/lc_2845_seonghui.cpp
@@ -6,30 +6,18 @@ using namespace std;
 class Solution {
 public:
 	long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
-		int result = 0, temp;
-		vector<int> validCnts;
-		unordered_map<int, int> dict;
-		
-		if (k == 0) validCnts.push_back(0);
-		
-		for (int i = 1; i <= nums.size(); i++) {
-			temp = 0;
-			for (int j = i; j <= nums.size(); j++) {
-				if (nums[j-1] % modulo == k) {
-					temp += 1;
-					if (temp % modulo == k) {
-						validCnts.push_back(temp);
-					}
-				}
-				if (dict.find(temp) == dict.end()) {
-					dict[temp] = 1;
-				}
-				else dict[temp] += 1;
-			}
-		}
+		long long result = 0, cnt = 0;
+		unordered_map<int, long long> dict;
 
-		for (int i = 0; i < validCnts.size(); i++) {
-			result += dict[validCnts[i]];
+		dict[0] = 1;
+		
+		for (int i = 0; i < nums.size(); i++) {
+			if (nums[i] % modulo == k) cnt += 1;
+
+			int mod = cnt % modulo;
+			int target = (mod - k + modulo) % modulo;
+			result += dict[target];
+			dict[mod] += 1;
 		}
 
 		return result;
