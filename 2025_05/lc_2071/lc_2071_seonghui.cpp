b875e0a54f5e9ac123b2b39e73de76aa21dbb6dc
Sincer1ty <87846251+Sincer1ty@users.noreply.github.com>
2025-05-05 16:06:29 +0900

    Update lc_2071_seonghui.cpp

--- 2025_05/lc_2071/lc_2071_seonghui.cpp
+++ 2025_05/lc_2071/lc_2071_seonghui.cpp
@@ -5,39 +5,41 @@ using namespace std;
 
 class Solution {
 public:
+    bool canAssign(int k, vector<int> tasks, vector<int> workers, int pills, int strength) {
+        workers = vector<int>(workers.end() - k, workers.end());
+
+        for (int i = k - 1; i >= 0; i--) {
+            // 큰 일꾼 k명
+            auto it = lower_bound(workers.begin(), workers.end(), tasks[i]);
+            if (it != workers.end()) {
+                workers.erase(it);
+            } else {
+                if (pills == 0) return false;
+                it = lower_bound(workers.begin(), workers.end(), tasks[i] - strength);
+                if (it == workers.end()) return false;
+                workers.erase(it);
+                pills--;
+            }
+        }
+
+        return true;
+    }
+
     int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
-        int count = 0, assigned = 0;
+        int count = 0;
 
         sort(workers.begin(), workers.end());
         sort(tasks.begin(), tasks.end());
         
-        for (int i = 0; i < workers.size(); i++) {
-            assigned = 0;
-            while (assigned < tasks.size() && workers[i] >= tasks[assigned]) {
-                assigned += 1;
-            }
-            
-            if (assigned == 0 || workers[i] < tasks[assigned - 1]) {
-                workers[i] += strength;
-            }
-            else {
-                count += 1;
-                // 불가능한 수로 만들기
-                workers[i] = -1 -strength;
-                tasks[assigned - 1] = 2000000001;
-            }
-        }
-        assigned = -1;
-        for (int i = 0; i < workers.size(); i++) {
-            for (int j = assigned + 1; j < tasks.size(); j++) {
-                // 있으면 할당 가능
-                if (workers[i] >= tasks[j]) {
-                    if (pills == 0) return count;
-                    count += 1;
-                    assigned = j;
-                    pills -= 1;       
-                    break;
-                }
+
+        int low = 0, high = min(tasks.size(), workers.size());
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (canAssign(mid, tasks, workers, pills, strength)) {
+                count = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
