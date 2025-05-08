8b2081ef61b80d8169a7b15eab3f1e7e60f20aa4
Techn0 <gnlrjswjd@naver.com>
2025-05-08 11:43:25 +0900

    Update lc_3341_JHwi.py

--- 2025_05/lc_3341/lc_3341_JHwi.py
+++ 2025_05/lc_3341/lc_3341_JHwi.py
@@ -6,13 +6,19 @@ class Solution:
         n = len(moveTime)
         m = len(moveTime[0])
 
+        heap = [(),(),()] # 현재 시간, x, y
+
         visited = [[False] * m for _ in range(n)]
 
         direction = [(0,1),(1,0),(-1,0),(0,-1)]
 
-        for i in range(n):
-            L  
+        while heap:
+            timeNow, x, y = heap
+            if visited[x][y] == True
+                continue
+            visited[x][y] = True
 
+            
 
 
 
@@ -21,5 +27,5 @@ class Solution:
 # 이동에는 1초 소요
 
 # 시간이 흘렀을 때 열리는 문중 가장 값이 작은 인덱스로 이동하는 조건문을 만들자
-# 해당[i][j], 오른쪽[i][j+1], 아래[i+1][j]
+# 해당[i][j], 오른쪽[i][j+1], 아래[i+1][j] ... 해서
 # 비교하면서 하면 되려나?
