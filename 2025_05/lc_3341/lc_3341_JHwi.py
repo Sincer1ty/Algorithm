0565d8b2d644e394db9240fa717e00996a54e8c8
Techn0 <gnlrjswjd@naver.com>
2025-05-08 11:38:04 +0900

    Update lc_3341_JHwi.py

--- 2025_05/lc_3341/lc_3341_JHwi.py
+++ 2025_05/lc_3341/lc_3341_JHwi.py
@@ -1,6 +1,17 @@
+import heapq
+
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
-        timeNow = 0
+        # timeNow = 0
+        n = len(moveTime)
+        m = len(moveTime[0])
+
+        visited = [[False] * m for _ in range(n)]
+
+        direction = [(0,1),(1,0),(-1,0),(0,-1)]
+
+        for i in range(n):
+            L  
 
 
 
@@ -8,5 +19,7 @@ class Solution:
 
 # 현지 시간을 카운트할 timeNow 변수 필요
 # 이동에는 1초 소요
+
 # 시간이 흘렀을 때 열리는 문중 가장 값이 작은 인덱스로 이동하는 조건문을 만들자
 # 해당[i][j], 오른쪽[i][j+1], 아래[i+1][j]
+# 비교하면서 하면 되려나?
