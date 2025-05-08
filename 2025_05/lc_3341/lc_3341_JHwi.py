fceb21349e216821ed025744f71887f049f18845
techn-0 <gnlrjswjd@naver.com>
2025-05-08 19:54:01 +0900

    Update lc_3341_JHwi.py

--- 2025_05/lc_3341/lc_3341_JHwi.py
+++ 2025_05/lc_3341/lc_3341_JHwi.py
@@ -2,33 +2,29 @@ import heapq
 
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
-        # timeNow = 0
         n = len(moveTime)
         m = len(moveTime[0])
 
-        heap = [(),(),()] # 현재 시간, x, y
+        heap = [(0, 0,0)] # 현재 시간, x, y
 
         visited = [[False] * m for _ in range(n)]
 
         direction = [(0,1),(1,0),(-1,0),(0,-1)]
 
         while heap:
-            timeNow, x, y = heap
-            if visited[x][y] == True
+            timeNow, x, y = heapq.heappop(heap)
+            if visited[x][y] == True:
                 continue
             visited[x][y] = True
 
             # 끝에 도착했다면 시간 리턴
             if x==n-1 and y == m-1:
                 return timeNow
+            
+            for dx, dy in direction:
+                nx = x + dx
+                ny = y + dy
 
-
-
-
-
-# 현지 시간을 카운트할 timeNow 변수 필요
-# 이동에는 1초 소요
-
-# 시간이 흘렀을 때 열리는 문중 가장 값이 작은 인덱스로 이동하는 조건문을 만들자
-# 해당[i][j], 오른쪽[i][j+1], 아래[i+1][j]
-# 비교하면서 하면 되려나?
+                if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
+                    next_time = max(timeNow, moveTime[nx][ny]) + 1
+                    heapq.heappush(heap, (next_time, nx, ny))
