977199f08d48082fac32b00141a8bdb90a16746c
techn-0 <gnlrjswjd@naver.com>
2025-05-13 18:44:54 +0900

    Update lc_2094_JHwi.py

--- 2025_05/lc_2094/lc_2094_JHwi.py
+++ 2025_05/lc_2094/lc_2094_JHwi.py
@@ -2,10 +2,21 @@ class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
         n = len(digits)
     
-        arr = []
-        for i in rangen(n):
-            for j in range(i, n):
-                
+        arr = []    
+        for i in range(n):
+            if digits[i] == 0:  # 첫자리 0 이면 ㅌㅌ
+                continue
+            for j in range(n):
+                if j == i:
+                    continue
+                for k in range(n):
+                    if k == i or k == j:
+                        continue
+                    if digits[k] % 2:
+                        continue
+                    num = digits[i] * 100 + digits[j] * 10 + digits[k]
+                    arr.append(num)
+        return sorted(list(set(arr)))
 
         
         
