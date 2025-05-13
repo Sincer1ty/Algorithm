531c52856d66aab46bdcd26136df8d08fc754715
Techn0 <gnlrjswjd@naver.com>
2025-05-13 16:07:07 +0900

    Create lc_2094_JHwi.py

---
+++ 2025_05/lc_2094/lc_2094_JHwi.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+    
+        arr = []
+        for i in rangen(n):
+            for j in range(i, n):
+                
+
+        
+        
