21fd083f455f23ec23672d7edbd1c9cdf57bf72e
techn-0 <gnlrjswjd@naver.com>
2025-05-19 19:08:28 +0900

    Create lc_3024_JHwi.py

---
+++ 2025_05/lc_3024/lc_3024_JHwi.py
@@ -0,0 +1,22 @@
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        c = nums.pop(nums.index(max(nums)))
+        b = nums.pop(nums.index(max(nums)))
+        a = nums.pop(nums.index(max(nums)))
+
+        print(a, b, c)
+
+        # 삼각형이 아니면
+        if a+b <= c:
+            return "none"
+        # 정삼각형인 경우
+        if a == b == c:
+            return "equilateral"
+        # 이등변 삼각형
+        elif a == b or a == c or b == c:
+            return "isosceles"
+        #걍 삼각형
+        return "scalene"
+
+        
+        
