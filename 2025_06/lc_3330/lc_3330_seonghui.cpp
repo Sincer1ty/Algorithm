1e052dcf00fe58052f9e68ec6267a75f5c6c8b19
Sincer1ty <woodsy0417@gmail.com>
2025-07-01 14:27:06 +0900

    Create lc_3330_seonghui.cpp

---
+++ 2025_06/lc_3330/lc_3330_seonghui.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int possibleStringCount(string word) {
+		int result = 1; // 자기 자신
+		for (int i = 1; i < word.size(); ++i) {
+			if (word[i - 1] == word[i]) ++result;;
+		}
+		return result;
+    }
+};
